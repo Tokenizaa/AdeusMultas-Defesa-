07d6eccff8e5e66c176b53ba7989872bfb490e45
Netto Farias <35587885+Tokenizaa@users.noreply.github.com>
2026-09-08 13:52:21 -0300

    feat(onboarding): expose canonical V2 route component

---
+++ src/onboarding-v2/ui/OnboardingV2Route.tsx
@@ -0,0 +1,8 @@
+import { useMemo } from 'react';
+import { OnboardingPage } from './OnboardingPage';
+import { createBrowserOnboardingHttpClient, createOnboardingHttpApplication } from '../application/http-application';
+
+export function OnboardingV2Route() {
+  const application = useMemo(() => createOnboardingHttpApplication(createBrowserOnboardingHttpClient()), []);
+  return <OnboardingPage application={application} />;
+}
